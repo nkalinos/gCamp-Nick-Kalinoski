61261fc3d183ef80a1b5ad73ba0695e1cb33bf43
Nick Kalinoski <nickkalinoski@Nicks-MacBook-Air.local>
2015-02-04 17:43:02 -0700

    user stories

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,58 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @users = User.all
+  end
+
+  def show
+   @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to users_path, notice: 'User was successfully created.' }
+        format.json { render :show, status: :created, location: @user }
+      else
+        format.html { render :new }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+    def update
+      @user = User.find(params[:id])
+      if @user.update(user_params)
+        redirect_to users_path, notice: 'User was successfully updated.'
+      else
+        render :edit
+      end
+    end
+
+
+    def destroy
+      @user.destroy
+
+        redirect_to users_url, notice: 'User was successfully deleted.'
+      end
+
+      private
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :email)
+    end
+end
