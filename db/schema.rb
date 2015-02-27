185b4604cc766f2d9f0e10ae5c122dd4e8ca376a
Nick Kalinoski <yogurtbrothers@gmail.com>
2015-02-27 16:31:39 -0700

    users can see task count on projects page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150226232602) do
+ActiveRecord::Schema.define(version: 20150227231510) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,7 +26,10 @@ ActiveRecord::Schema.define(version: 20150226232602) do
     t.datetime "updated_at",  null: false
     t.date     "date"
     t.boolean  "box"
+    t.integer  "project_id"
   end
+
+  add_index "tasks", ["project_id"], name: "index_tasks_on_project_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
@@ -37,4 +40,5 @@ ActiveRecord::Schema.define(version: 20150226232602) do
     t.string   "password_digest"
   end
 
+  add_foreign_key "tasks", "projects"
 end
