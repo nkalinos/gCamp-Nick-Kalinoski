4a27250c6e462f1a6284db1c100b226c715f42de
Nick Kalinoski <yogurtbrothers@gmail.com>
2015-03-09 15:04:02 -0600

    remove end

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,7 +31,7 @@ class UsersController < ApplicationController
          render :new
       end
     end
-  end
+  
 
     def update
       @user = User.find(params[:id])
@@ -57,4 +57,6 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:first_name, :last_name, :email, :password)
     end
+
+
 end
