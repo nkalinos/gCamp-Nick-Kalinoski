18dc90ebaa52f71b977155a287fa455282be19ed
Nick Kalinoski <yogurtbrothers@gmail.com>
2015-04-15 21:15:45 -0600

    user data is deleted when removing user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   validates :first_name, :last_name, presence: true
   validates :email, presence: true, uniqueness: true
   has_many :comments, dependent: :destroy
-  has_many :memberships
+  has_many :memberships, dependent: :destroy
   has_many :projects, through: :memberships
   def full_name
     first_name + " " + last_name
--- db/migrate/20150409211817_change_membership_role.rb
+++
@@ -1,5 +0,0 @@
-class ChangeMembershipRole < ActiveRecord::Migration
-  def change
-    rename_column :memberships, :role_id, :role
-  end
-end
